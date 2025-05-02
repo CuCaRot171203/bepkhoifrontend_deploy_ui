06eb2a984de6c0e7c8a79b7b9d94e76babea7777
CuCaRot171203 <tungnguyenthanh710@gmail.com>
2025-05-03 05:39:20 +0700

    code commit fix design

--- src/layouts/ManageLayout.tsx
+++ src/layouts/ManageLayout.tsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeaderManage from "../components/Manage/HeaderManage";
 import { Outlet } from "react-router-dom";
 
 const ManageLayout: React.FC = () => {
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  if (isMobile) {
+    return (
+      <div className="min-h-screen w-full flex items-center justify-center bg-[#faedd7] text-center text-gray-800 text-[2.1vw] p-[2vw]">
+        Màn hình này không thể hoạt động trên thiết bị di động
+      </div>
+    );
+  }
+
   return (
     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
       <HeaderManage />
--- src/layouts/ShopLayout.tsx
+++ src/layouts/ShopLayout.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useEffect, useState } from "react";
 import HeaderShop from "../components/Shop/HeaderShop";
 import FooterShop from "../components/Shop/FooterShop";
 import { Outlet } from "react-router-dom";
@@ -10,6 +10,26 @@ interface ShopLayoutProps {
 
 const ShopLayout: React.FC<ShopLayoutProps> = ({ modelMode }) => {
   const [activeTab, setActiveTab] = useState("home");
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkMobile = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+
+    checkMobile();
+    window.addEventListener("resize", checkMobile);
+    return () => window.removeEventListener("resize", checkMobile);
+  }, []);
+
+  if (!isMobile) {
+    return (
+      <div className="min-h-screen w-full flex items-center justify-center bg-[#faedd7] text-center text-gray-800 text-[0.9vw] p-[2vw]">
+        Phiên bản máy tính bàn hiện không hỗ trợ đặt món. Vui lòng sử dụng thiết
+        bị di động hoặc máy tính bảng!
+      </div>
+    );
+  }
   return (
     <div className="min-h-screen w-full bg-gray-100 flex flex-col">
       <div className="sticky top-0 z-50 w-full">

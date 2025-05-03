f167b2f44a6fb9ce72619eb659b1c64480e517aa
CuCaRot171203 <tungnguyenthanh710@gmail.com>
2025-05-04 04:27:08 +0700

    code commit fix

--- src/components/LandingPage/FloatingContactButtons.tsx
+++ src/components/LandingPage/FloatingContactButtons.tsx
@@ -18,7 +18,7 @@ const FloatingContactButtons = () => {
 
       {/* Messenger */}
       <a
-        href="https://m.me/yourpage"
+        href="https://m.me/nhahangbepkhoi.op1"
         target="_blank"
         rel="noopener noreferrer"
         className="w-[3.5vw] h-[3.5vw] bg-blue-600 text-white rounded-full shadow-lg flex items-center justify-center hover:scale-105 transition"
@@ -38,7 +38,7 @@ const FloatingContactButtons = () => {
 
       {/* Calling */}
       <a
-        href="tel:0912345678"
+        href="tel:0975307087"
         className="w-[3.5vw] h-[3.5vw] bg-green-500 text-white rounded-full shadow-lg flex items-center justify-center hover:scale-105 transition"
       >
         <PhoneOutlined />
